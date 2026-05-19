21dc325d998c9273e34c93dc8ea2062805a39f0a
Eli <153844922+EliBellah@users.noreply.github.com>
2026-05-19 18:08:04 +0200

    apparition cube
    script on click , changement de fond

---
+++ Assets/Scripts/OpenOnClick.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class OpenOnClick : MonoBehaviour
+{
+   public GameObject porte;
+   public GameObject fenetre;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        UnityEngine.Debug.Log("pipicaca");
+    }
+
+        void OnMouseDown()
+    {
+        Instantiate(fenetre);
+    }
+}
